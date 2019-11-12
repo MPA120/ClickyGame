e295b5ceeda5dcdb9b04490f1d78cda8dc1bd4e1
MPA <50469825+MPA120@users.noreply.github.com>
2019-11-12 17:58:34 -0500

    functional

--- clickygame/src/App.js
+++ clickygame/src/App.js
@@ -1,26 +1,130 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import MatchCard from "./components/MatchCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import matches from "./cards.json";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let correctGuesses = 0;
+let bestScore = 0;
+let clickMessage = "Click on a devlogo to start the game! Do not click the same one twice.";
+
+class App extends Component {
+    
+    
+    state = {
+        matches,
+        correctGuesses,
+        bestScore,
+        clickMessage
+    };
+
+    setClicked = id => {
+
+        
+        const matches = this.state.matches;
+
+        // Filter for the clicked match
+        const clickedMatch = matches.filter(match => match.id === id);
+
+        
+        if (clickedMatch[0].clicked){
+
+            console.log ("Correct Guesses: " + correctGuesses);
+            console.log ("Best Score: " + bestScore);
+
+            correctGuesses = 0;
+            clickMessage = "Game Over, You already clicked that one!"
+
+            for (let i = 0 ; i < matches.length ; i++){
+                matches[i].clicked = false;
+            }
+
+            this.setState({clickMessage});
+            this.setState({ correctGuesses });
+            this.setState({matches});
+
+        
+        } else if (correctGuesses < 11) {
+
+            // Set its value to true
+            clickedMatch[0].clicked = true;
+
+            // increment the appropriate counter
+            correctGuesses++;
+            
+            clickMessage = "Keep Going!";
+
+            if (correctGuesses > bestScore){
+                bestScore = correctGuesses;
+                this.setState({ bestScore });
+            }
+
+            
+            matches.sort(function(a, b){return 0.5 - Math.random()});
+
+            
+            this.setState({ matches });
+            this.setState({correctGuesses});
+            this.setState({clickMessage});
+        } else {
+
+            
+            clickedMatch[0].clicked = true;
+
+            
+            correctGuesses = 0;
+
+            
+            clickMessage = "You Win!";
+            bestScore = 12;
+            this.setState({ bestScore });
+            
+            for (let i = 0 ; i < matches.length ; i++){
+                matches[i].clicked = false;
+            }
+
+            // Shuffle the array to be rendered in a random order
+            matches.sort(function(a, b){return 0.5 - Math.random()});
+
+            // Set this.state.matches equal to the new matches array
+            this.setState({ matches });
+            this.setState({correctGuesses});
+            this.setState({clickMessage});
+
+        }
+    };
+
+    render() {
+        return (
+            <Wrapper>
+                <Title>DevLogo Match Game</Title>
+        
+                <h3 className="scoreSummary">
+                    {this.state.clickMessage}
+                </h3>
+                
+                <h3 className="scoreSummary card-header">
+                    Correct Guesses: {this.state.correctGuesses} 
+                    <br />
+                    Best Score: {this.state.bestScore} 
+                </h3>
+                <div className="container">
+                <div className="row">
+                {this.state.matches.map(match => (
+                    <MatchCard
+                        setClicked={this.setClicked}
+                        id={match.id}
+                        key={match.id}
+                        image={match.image}
+                    />
+                ))}
+                </div>
+                </div>
+
+            </Wrapper>
+        );
+    }
 }
 
 export default App;
